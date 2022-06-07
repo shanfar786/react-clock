de5c60b835bf9734c58d7fe38c9e701cfc541893
shanfar786 <43869550+shanfar786@users.noreply.github.com>
2022-06-07 08:39:45 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const d = new Date("2015-03-25");
+  const currentDateTime =d.toLocaleString()
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        
+          <h2>{currentDateTime}</h2>
+        
+       
       </header>
     </div>
   );
